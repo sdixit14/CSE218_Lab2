535467d3d75e73ce6777dd8233ba3e252b126658
sdixit14 <93486784+sdixit14@users.noreply.github.com>
2022-10-07 09:43:06 -0700

    Add files via upload

---
+++ AutoflashLED.py
@@ -0,0 +1,23 @@
+﻿import RPi.GPIO as GPIO  
+ import time  
+
+ led = 18  
+ GPIO.setmode( GPIO.BOARD)  
+ GPIO.setup( led, GPIO.OUT)  
+  
+ pwm_led = GPIO.PWM( led, 50)  
+  
+ pwm_led.start(100)
+  
+ try:  
+   while True:  
+     duty_s = input("Enter Brightness Value (0 to 100):")  
+     duty = int(duty_s)  
+     pwm_led.ChangeDutyCycle(duty)  
+     time.sleep(0.5)  
+ except KeyboardInterrupt:  
+   print("Exiting Program") 
+ except:  
+   print("Error Occurs, Exiting Program")
+ finally:  
+   GPIO.cleanup() 
